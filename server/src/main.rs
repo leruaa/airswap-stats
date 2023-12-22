6c3e033bc74887437227e2ceeee557fa74252f9d
leruaa <aurelien.catinon@gmail.com>
2023-12-22 09:59:59 +0000

    Add logs on requests

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,9 +14,20 @@ use config::Config;
 use pool::Pool;
 use provider::Providers;
 use shuttle_secrets::{SecretStore, Secrets};
+use tower_http::trace::{DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::{level_filters::LevelFilter, Level};
+use tracing_subscriber::EnvFilter;
 
 #[shuttle_runtime::main]
 async fn axum(#[Secrets] secret_store: SecretStore) -> shuttle_axum::ShuttleAxum {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
     let config = Config::get();
 
     let pools = config.pools.iter().map(Pool::from).collect::<Vec<_>>();
@@ -25,7 +36,13 @@ async fn axum(#[Secrets] secret_store: SecretStore) -> shuttle_axum::ShuttleAxum
     let router = Router::new()
         .route("/pools/assets", get(services::pools::assets))
         .route("/staking/share", get(services::staking::share))
-        .with_state((Arc::new(providers), Arc::new(pools)));
+        .with_state((Arc::new(providers), Arc::new(pools)))
+        .layer(
+            TraceLayer::new_for_http()
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO))
+                .on_failure(DefaultOnFailure::new()),
+        );
 
     Ok(router.into())
 }
